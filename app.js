14675cd6894aba6f374e6ea962d3bdb473e339d6
Caio Maqueise Alécio Pinheiro <caio.maqueise@gmail.com>
2020-11-17 22:13:11 -0400

    Conexão com Banco de Dados e CRUD Cliente

--- app.js
+++ app.js
@@ -1,10 +1,20 @@
+const { response } = require('express')
 const express = require('express')
 const nunjucks = require('nunjucks')
+const {db} = require('./src/db/connection')
+
+/* db.query('SELECT * FROM cliente',(err,result)=>{
+  if(err){
+    console.log(`Houve um erro ao conectar: ${err}`)
+  }
+  console.log(result.rows)
+}) */
 
 
 const app = express()
 const port = 3000
-app.use(express.static('public'));
+app.use(express.static('public'))
+app.use(express.urlencoded({extended:true}))
 app.set('view engine','.html')
 
 nunjucks.configure('./src/views', {
@@ -22,14 +32,35 @@ app.get('/categoria-produto/listar',(req,res)=>{
 app.get('/categoria-produto/adicionar',(req,res)=>{
   res.render('categoria-produto/adicionar')
 })
+// ROTAS PARA CADASTRO DE CLIENTES
+
 app.get('/cliente/listar',(req,res)=>{
-  let {clientes} = require('./src/db/fakeData')
-  res.render('cliente/listar',{clientes:clientes})
+  db.query('SELECT * FROM cliente',(err,result)=>{
+    if(err){
+      console.log(`Houve um erro ao listar os clientes: ${err}`)
+    }
+    res.render('cliente/listar',{clientes:result.rows})
+  })
+
+  
 })
 app.get('/cliente/adicionar',(req,res)=>{
   res.render('cliente/adicionar')
 })
+app.post('/cliente/salvar',(req,res)=>{
+  const query = {
+    text:'INSERT INTO cliente(nome,cpf) VALUES ($1,$2)',
+    values:[req.body.nome,req.body.cpf]
+  }
+  db.query(query,(err,result)=>{
+    if(err){
+      console.log(`Houve um erro ao inserir o cliente: ${err}`)
+    }
+    console.log(result)
+  })
 
+  res.redirect('/cliente/listar')
+})
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
